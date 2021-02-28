000d71a4362f41f4582241b1ad55e0b9af1a0516
krvvko <krvvko@gmail.com>
2021-02-28 04:09:53 -0400

    Escape From Tarkov content added

--- articles/3.php
+++ articles/3.php
@@ -1,4 +1,152 @@
 <br>
 <br>
 <br>
-4
+
+<style>
+
+
+</style>
+
+<p>About Escape From Tarkov:</p>
+
+<div class="ArticleSectionsText">
+
+    <div class="ArticleSectionsTextContent">
+
+        <h4>
+            <a href="https://www.escapefromtarkov.com/?lang=en" target="_blank" class="ArticleContent">
+                Escape from Tarkov
+            </a>
+            is a first-person multiplayer online role-playing game developed by Battlestate Games,
+            which combines the genres of FPS, combat simulator and RPG with MMO elements. Escape from Tarkov is
+            developed
+            and published by Battlestate Games. A game based on the Unity engine bla bla bla....
+        </h4>
+
+    </div>
+
+    <img src="/resources/img/avtory-escape-from-tarkov-izvinilis-za-mnenie-chto-zhenshhinam-ne-mesto-na-vojne-ego-vyskazali-tri-goda-nazad.jpg"
+         width="665px" style="margin-bottom: 15px; border-radius: 5px">
+
+    <div class="ArticleSectionsTextContent">
+
+        <h4>
+            Escape from Tarkov actually is an open beta project, but It's already playable and selling for the full
+            price (60$-155$ in US).
+            In that game you should collect trash, that spawns somewhere on location, and fight with AI (scavs) and real
+            players (PMC's and scav-players)
+            <br>
+            <br>
+            There are 2 stages in the game:
+            <br>
+            <br>
+
+
+            <span class="ArticleMargin">
+
+                1 -
+                <a href="https://escapefromtarkov.gamepedia.com/Gameplay_modes" target="_blank" class="ArticleContent">
+                    An actual gameplay.
+                </a>
+                    When you are deploying on the locations, and then doing whatever you want, you can loot,
+                fight with other players or AI, roam on the location and.... that's it
+
+                <br>
+                <br>
+
+                2 -
+                <a href="https://escapefromtarkov.gamepedia.com/Hideout" target="_blank" class="ArticleContent">
+                    Stash
+                </a>
+                    (Inventory) sort. You will also spend much time selling/buying stuff, and sorting your stash
+                (especially if you got 60$ version, because there is a smaller stash there)
+
+            </span>
+
+
+        </h4>
+
+    </div>
+
+    <img src="/resources/img/Escape-from-Tarkov.jpg"
+         width="665px" style="margin-bottom: 15px; border-radius: 5px">
+
+
+    <div class="ArticleSectionsTextContent">
+
+        <h4>
+
+            <span class="ArticleMiddle">More about 1st stage: </span>
+            <br>
+
+            There are many locations, with different loot, different time, different size, and different amount of
+            players on them.
+
+            <br>
+            <br>
+
+            Shortly about evey of them available at this moment:
+
+            <br>
+            <br>
+
+            <a href="https://escapefromtarkov.gamepedia.com/Customs" target="_blank" class="ArticleContent">Customs:</a>
+            <br>
+            <span class="ArticleMargin">
+                The location mostly for beginners, because there is no that much expensive loot there, and many early-beginning quests.
+            </span><br>
+            <a href="https://escapefromtarkov.gamepedia.com/Factory" target="_blank"
+               class="ArticleContent">Factory: </a>
+            <br>
+            <span class="ArticleMargin">
+                A really small map, that has a very poor loot, and with 5-6 players on it. The location made for PVP and quests on it.
+                It's a good location to learn how shooting in this game works.
+            </span><br>
+            <a href="https://escapefromtarkov.gamepedia.com/Reserve" target="_blank" class="ArticleContent">Reserve:</a>
+            <br>
+            <span class="ArticleMargin">
+                A military base. The location is complex and huge, has lots of underground places, but also has a very very good loot on it.
+                There is 1 problem that makes this location not that amazing or looting - you need lots of keys, because the best loot on
+                this location is usually behind the locked doors. There are many good and experienced players on the location, so it's
+                not great for beginners.
+            </span><br>
+            <a href="https://escapefromtarkov.gamepedia.com/Woods" target="_blank" class="ArticleContent">Woods:</a>
+            <br>
+            <span class="ArticleMargin">
+                A huge forest, that contain lot's of players that are hiding in the bushes, usually people take guns with sniper scopes,
+                because the fight distance is 100+ meters almost everytime. Not that much loot here, but there might be a boss, who can
+                have expensive stuff.
+            </span><br>
+            <a href="https://escapefromtarkov.gamepedia.com/Shoreline" target="_blank"
+               class="ArticleContent">Shoreline:</a>
+            <br>
+            <span class="ArticleMargin">
+                Also a 90% forest and field map, but the last 10% is a sanatorium, that contains LOTS of great and expensive stuff,
+                which is hidden behind lock doors, so, you need lot's of keys to loot everything. The location is great for making
+                in game money, and fighting in close and long ranges, but usually there are a lot high geared players, so, not that
+                great for newcomers.
+            </span><br>
+            <a href="https://escapefromtarkov.gamepedia.com/Interchange" target="_blank" class="ArticleContent">Interchange:</a>
+            <br>
+            <span class="ArticleMargin">
+                A huge mall with lots of shops in it, there might be a good loot in some of them. Usually not that much geared players
+                in it, and lot's of scav-players, good for looting, and newcomer friendly map.
+            </span><br>
+            <a href="https://escapefromtarkov.gamepedia.com/The_Lab" target="_blank" class="ArticleContent">The Lab:</a>
+            <br>
+            <span class="ArticleMargin">
+                The most dangerous place in entire game, there are no safe places, and no ways to run, if you meet a player - you
+                have to fight with him, and there will be only 1 winner. Requires a labs 1 time used key card, which is expensive,
+                and you are loosing it after entering the location. The only AI there - raiders, high geared scaves with increased
+                aim. If you survive on this location - you will become really rich. Not newcomer friendly location.
+            </span>
+
+
+        </h4>
+
+    </div>
+
+    <img src="/resources/img/Map.png"
+         width="665px" height="500px" style="margin-bottom: 15px; border-radius: 5px">
+
+</div>
