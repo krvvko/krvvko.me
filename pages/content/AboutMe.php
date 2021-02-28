cdeddb594cd3fe4b3b7de645f6c987a43739b945
krvvko <krvvko@gmail.com>
2021-02-28 01:29:32 -0400

    Small changes + html block was added

--- pages/content/AboutMe.php
+++ pages/content/AboutMe.php
@@ -10,19 +10,22 @@
 
         <h4>
 
-            Here I am - the guy who really enjoys coding and creating.  I am already
+            Here I am - the guy who really enjoys coding and creating. I am already
 
             <?php
 
             echo (new DateTime())->diff(new DateTime('2001-09-26 12:35:54'))->y;
 
-            ?> years old (btw my age text was created with the <a style="text-decoration: none" href="https://en.wikipedia.org/wiki/PHP"><span class="ArticleContent"> ?php </span></a>script, it's not just a text :D )
+            ?> years old (btw my age text was created with the <a style="text-decoration: none"
+                                                                  href="https://en.wikipedia.org/wiki/PHP"><span
+                        class="ArticleContent"> ?php </span></a>script, it's not just a text :D )
 
         </h4>
 
     </div>
 
-    <img style="display: block; margin-left: auto; margin-right: auto; width: 30%; margin-bottom: 15px" src="/resources/media/ele-running.gif" alt="" >
+    <img style="display: block; margin-left: auto; margin-right: auto; width: 30%; margin-bottom: 15px"
+         src="/resources/media/ele-running.gif" alt="">
 
     <div class="ArticleSectionsTextContent">
 
@@ -31,16 +34,21 @@
             As you probably understand, my main hobby - coding (and a little bit designing)
             I also a really big enjoyer of cats and shooter videogames like
 
-            <span><a style="color: #b3b3b3" href="https://www.ubisoft.com/en-gb/game/rainbow-six/siege" class="ArticleContent">Rainbow Six Siege</a></span> ,
+            <span><a target="_blank" style="color: #b3b3b3" href="https://www.ubisoft.com/en-gb/game/rainbow-six/siege"
+                     class="ArticleContent">Rainbow Six Siege</a></span> ,
 
-            <span><a style="color: #B3B3B3" href="https://store.steampowered.com/app/730/CounterStrike_Global_Offensive/" class="ArticleContent">CS:GO</a></span> and
+            <span><a target="_blank" style="color: #B3B3B3"
+                     href="https://store.steampowered.com/app/730/CounterStrike_Global_Offensive/"
+                     class="ArticleContent">CS:GO</a></span> and
 
-            <span><a style="color: #B3B3B3" href="https://www.escapefromtarkov.com/?lang=en" class="ArticleContent">Escape From Tarkov</a></span>.
+            <span><a target="_blank" style="color: #B3B3B3" href="https://www.escapefromtarkov.com/?lang=en"
+                     class="ArticleContent">Escape From Tarkov</a></span>.
 
             <br>
             <br>
 
-            All this game are great, except <span style="color: #B3B3B3" class="ArticleContent">CS:GO</span>, because this game is really bad.
+            All this games are great, except <span style="color: #B3B3B3" class="ArticleContent">CS:GO</span>, because
+            this game is really bad.
             Also <span style="color: #B3B3B3" class="ArticleContent">Rainbow Six Siege</span> is really bad.
             And also <span style="color: #B3B3B3" class="ArticleContent">Escape From Tarkov</span> is trash.
             Ok, nvm, all this games are bad, but I still like them.
@@ -51,13 +59,33 @@
 
     </div>
 
-    <img style="display: block; border-radius: 0 0 5px 5px; margin: -25px auto 20px;" src="/resources/img/AboutMeGames.png" alt="" >
+    <img style="display: block; border-radius: 0 0 5px 5px; margin: -25px auto 20px;"
+         src="/resources/img/AboutMeGames.png" alt="">
 
     <div class="ArticleSectionsTextContent">
 
         <h4>
 
-            Cats Are Awesome.
+
+
+            &lt;!DOCTYPE html&gt;
+            <br>
+            &lt;html&gt;
+            <br><br>
+            &nbsp;&nbsp;&nbsp;&lt;head&gt;
+            <br>
+            &nbsp;&nbsp;&nbsp;&lt;/head&gt;
+            <br><br>
+            &nbsp;&nbsp;&nbsp;&lt;body&gt;
+            <br>
+            <br>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &lt;h1&gt;Cats Are Awesome.&lt;/h1&gt;
+            <br>
+            <br>
+            &nbsp;&nbsp;&nbsp;&lt;/body&gt;
+            <br><br>
+            &lt;/html&gt;
+
 
         </h4>
 
