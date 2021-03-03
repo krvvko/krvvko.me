e31fc2146ab936faaa16b2cdc3b527825ec22626
krvvko <krvvko@gmail.com>
2021-03-03 14:56:51 -0400

    -

--- articles/0.php
+++ articles/0.php
@@ -1,4 +1,25 @@
 <br>
 <br>
 <br>
-1
+
+<style>
+
+
+</style>
+
+<p>How I'm Creating YouTube Videos:</p>
+
+<div class="ArticleSectionsText">
+
+    <div class="ArticleSectionsTextContent">
+
+        <h4>
+
+            The video creating is hard only at the beginning, when you don't know how to use programs, and what to do with them,
+            but every time you will use them - you will get more experience
+
+        </h4>
+
+    </div>
+
+</div>
