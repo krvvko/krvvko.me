e5e99aea8092ef8a30f27ca88b19464ec8656bbb
krvvko_huh <krvvko@gmail.com>
2022-01-23 23:35:27 -0500

    updated tests

---
+++ database/migrations/2022_01_24_043152_create_articles_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+            $table->string('img');
+            $table->date('date');
+            $table->string('description');
+            $table->string('title');
+            $table->integer('views')->default(0);
+            $table->text('content_EN');
+            $table->text('content_RU');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
