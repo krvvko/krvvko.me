8073e01a878c3d2cdc11c44ca66352a479eb912b
Krevvetka <krvvko@gmail.com>
2022-10-26 02:50:13 -0400

    animations added

--- src/components/Title/index.jsx
+++ src/components/Title/index.jsx
@@ -1,6 +1,6 @@
 import './index.css'
 import {Helmet} from "react-helmet";
-import React from "react";
+import {useEffect} from "react";
 
 const Title = () => {
 
@@ -9,13 +9,15 @@ const Title = () => {
 
     const handleClick = async event => {
         const box = event.currentTarget;
+        const container = event.currentTarget.parentElement;
         document.getElementById("root").style.height = "100%";
         setTimeout(() => {
+            container.style.display = 'none';
             box.style.display = 'none';
         }, 700);
     };
 
-
+    // setting title
     function setTitle() {
         let t = document.location.href.split('/');
 
@@ -28,7 +30,7 @@ const Title = () => {
                 return "Bookmarks";
             case "contacts#main":
             case "contacts":
-                return "Contact Me";
+                return "Contact";
             case "creation#main":
             case "creation":
                 return "Creation";
@@ -37,16 +39,51 @@ const Title = () => {
         }
     }
 
+    // animation
+    let i = 0;
+    let string = "cd " + setTitle() + "/ ";
+    let newString = "";
+    const printStr = setInterval(function () {
+        newString += string[i++];
+        document.getElementById("code").innerHTML = newString + "</br>";
+        if (i >= string.length) {
+            clearInterval(printStr);
+        }
+    }, 100);
+
+    // checking if link has #main
+    const hrefChecker = document.location.href.split('#');
+    let disableWriter = {};
+    if(hrefChecker[hrefChecker.length-1] === 'main') {
+        document.getElementById("root").style.height = "100%";
+        disableWriter = {display: 'none'}
+    }
+
     return (
         <>
             <Helmet>
-                <title>{setTitle()+` - krvvko`}</title>
+                <title>{setTitle() + ` - krvvko`}</title>
             </Helmet>
-            <a href="#main" className="creation-header" onClick={handleClick}>
-                <h1 className="no-before">{setTitle()}</h1>
-            </a>
-        </>
+            <div className="creation-header" style={disableWriter}>
+                <a href="#main" className="creation-header-a" onClick={handleClick}>
+                    <div className="main-anim-container">
+                        <div className="anim-container">
+                            <h3 className="root-anim">root@krvvko-me:~#</h3>
+                            <div className="code-blink-js">
+                                <h3 id="code"></h3>
+                                <h3 className="blink">|</h3>
+                            </div>
 
+                        </div>
+                        <div className="anim-container invisible">
+                            <h3 className="root-anim">root@krvvko-me:~#</h3>
+                            <h3>cd {setTitle()}/|</h3>
+                        </div>
+                    </div>
+
+                </a>
+            </div>
+        </>
     )
 }
 
