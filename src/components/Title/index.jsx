faec978716cc2df8a1c8e27407ee53c392a91d77
krvvko <krvvko@gmail.com>
2022-10-27 09:42:04 -0400

    animation updated

--- src/components/Title/index.jsx
+++ src/components/Title/index.jsx
@@ -2,53 +2,80 @@ import './index.css'
 import {Helmet} from "react-helmet";
 
 const Title = () => {
-
+    let randomN = Math.floor(Math.random() * 1000);
     document.getElementById("root").style.height = "100vh";
     document.getElementById("root").style.overflowY = "hidden";
+    let counterClick = 0;
 
     const handleClick = async event => {
+        counterClick++;
+        console.log(counterClick);
         const box = event.currentTarget;
         const container = event.currentTarget.parentElement;
-        document.getElementById("blinkId").style.display = "none";
-        let timeout = 1700;
-
-        function meow(elId, strP) {
-            let i1 = 0;
-            let newString1 = "";
-            let stringL = strP;
-            timeout += stringL.length*60;
-            const printStr = setInterval(function () {
-                newString1 += stringL[i1++];
-                document.getElementById(elId).innerHTML = newString1 + "</br>";
-                if (i1 >= stringL.length) {
-                    clearInterval(printStr);
-                }
-            }, 60);
-        }
-
-        document.getElementById("line3").style.color = "#ec4e17";
-        setTimeout(()=>{
-            meow("line3","Meow")
-            document.getElementById("line2").style.color = "#99ec8f";
-            timeout += 5000;
-        },5000);
-        setTimeout(()=>{
-            document.getElementById("line1").style.color = "#99ec8f";
-            meow("line2","Rendering a result")
-            timeout += 3000;
-        },3000)
-        timeout += 3000;
-        meow("line1","Building an optimized output")
-
-        setTimeout(() => {
+        if (counterClick >= 2) {
             document.getElementById("root").style.height = "100%";
             document.location.hash = "#main";
-        }, timeout)
-        setTimeout(() => {
-            container.style.display = 'none';
-            box.style.display = 'none';
-        }, timeout+700);
-    };
+            setTimeout(() => {
+                container.style.display = 'none';
+                box.style.display = 'none';
+            }, 700);
+        } else {
+            document.getElementById("blinkId").style.display = "none";
+            document.getElementById(`line1${randomN}`).style.display = "inline";
+            document.getElementById(`line2${randomN}`).style.display = "inline";
+            document.getElementById(`line3${randomN}`).style.display = "inline";
+            if (counterClick >= 2) {
+                document.getElementById("root").style.height = "100%";
+                document.location.hash = "#main";
+                setTimeout(() => {
+                    container.style.display = 'none';
+                    box.style.display = 'none';
+                    document.getElementById(`line1${randomN}`).style.display = "none";
+                    document.getElementById(`line2${randomN}`).style.display = "none";
+                    document.getElementById(`line3${randomN}`).style.display = "none";
+                }, 700);
+            } else {
+                let timeout = 1700;
+
+                function meow(elId, strP) {
+                    let i1 = 0;
+                    let newString1 = "";
+                    let stringL = strP;
+                    timeout += stringL.length * 60;
+                    const printStr = setInterval(function () {
+                        newString1 += stringL[i1++];
+                        document.getElementById(elId).innerHTML = newString1 + "</br>";
+                        if (i1 >= stringL.length) {
+                            clearInterval(printStr);
+                        }
+                    }, 60);
+                }
+
+                document.getElementById(`line3${randomN}`).style.color = "#ec4e17";
+                setTimeout(() => {
+                    meow(`line3${randomN}`, "Meow")
+                    document.getElementById(`line2${randomN}`).style.color = "#99ec8f";
+                    timeout += 5000;
+                }, 5000);
+                setTimeout(() => {
+                    document.getElementById(`line1${randomN}`).style.color = "#99ec8f";
+                    meow(`line2${randomN}`, "Rendering a result")
+                    timeout += 3000;
+                }, 3000)
+                timeout += 3000;
+                meow(`line1${randomN}`, "Building an optimized output")
+
+                setTimeout(() => {
+                    document.getElementById("root").style.height = "100%";
+                    document.location.hash = "#main";
+                }, timeout)
+                setTimeout(() => {
+                    container.style.display = 'none';
+                    box.style.display = 'none';
+                }, timeout + 700);
+            }
+        }
+    }
 
     // setting title
     function setTitle() {
@@ -87,7 +114,7 @@ const Title = () => {
     // checking if link has #main
     const hrefChecker = document.location.href.split('#');
     let disableWriter = {};
-    if(hrefChecker[hrefChecker.length-1] === 'main') {
+    if (hrefChecker[hrefChecker.length - 1] === 'main') {
         document.getElementById("root").style.height = "100%";
         disableWriter = {display: 'none'}
     }
@@ -107,9 +134,9 @@ const Title = () => {
                                 <h3 id="blinkId" className="blink">|</h3>
                             </div>
                         </div>
-                        <h3 className="anim-lines" id="line1"></h3>
-                        <h3 className="anim-lines" id="line2"></h3>
-                        <h3 className="anim-lines" id="line3"></h3>
+                        <h3 className="anim-lines" style={{display: "none"}} id={'line1' + randomN}></h3>
+                        <h3 className="anim-lines" style={{display: "none"}} id={'line2' + randomN}></h3>
+                        <h3 className="anim-lines" style={{display: "none"}} id={'line3' + randomN}></h3>
                         <div className="anim-container invisible">
                             <h3 className="root-anim">root@krvvko-me:~#</h3>
                             <h3>cd {setTitle()}/|</h3>
