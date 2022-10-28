6fdf384f687f37b4c8ae9168c1236e11a9e10121
krvvko <krvvko@gmail.com>
2022-10-27 21:06:18 -0400

    huh?

--- src/components/Contacts/index.jsx
+++ src/components/Contacts/index.jsx
@@ -5,12 +5,16 @@ import React from "react";
 const Contacts = () => {
     return (
         <div className="d-container">
+
             <Title />
+
             <h1>Contact</h1>
+
             <span className="date">Oct 25, 2022</span>
             <p>This page needs no further explanation of what you can find here.</p>
             <br/>
             <p>If for any reason you want to contact me, then here you can find the best ways to do this.</p>
+            <h3>Page is not completely done yet</h3>
         </div>
     )
 }
