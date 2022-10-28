3b8d1706ba965462a7958d2673281131fb6901a8
Krevvetka <krvvko@gmail.com>
2022-10-28 01:23:57 -0400

    content optimized and updated

--- src/components/NotFound/index.jsx
+++ src/components/NotFound/index.jsx
@@ -3,62 +3,72 @@ import './index.css';
 
 const NotFound = () => {
 
-    let i = 0;
-    let stringP = " 404"
-    let newString = "";
+    function repeter() {
+        let i = 0;
+        let stringP = " 404"
+        let newString = "";
 
-    const printStr = setInterval(function () {
-        newString += stringP[i++];
-        document.getElementById("404").innerHTML = newString;
-        if (i >= stringP.length) {
-            clearInterval(printStr);
-            setTimeout(function () {
-                let iS = stringP.length;
-                let newStringS = "";
-                const printStrS = setInterval(function () {
-                    newStringS = stringP.substring(0, iS--);
-                    document.getElementById("404").innerHTML = newStringS;
-                    if (iS <= 0) {
-                        setTimeout(function () {
-                            let i = 0;
-                            stringP = " Not Found";
-                            newString = "";
-                            const printStr = setInterval(function () {
-                                newString += stringP[i++];
-                                document.getElementById("404").innerHTML = newString;
-                                if (i >= stringP.length) {
-                                    setTimeout(function () {
-                                        let iS = stringP.length;
-                                        let newStringS = "";
-                                        const printStrS = setInterval(function () {
-                                            newStringS = stringP.substring(0, iS--);
-                                            document.getElementById("404").innerHTML = newStringS;
-                                            if (iS <= 0) {
-                                                clearInterval(printStrS);
-                                            }
-                                        }, 140);
-                                    }, 3000)
-                                    clearInterval(printStr);
-                                }
-                            }, 120);
-                        }, 500)
-                        clearInterval(printStrS);
-                    }
-                }, 140);
+        const printStr = setInterval(function () {
+            newString += stringP[i++];
+            document.getElementById("404").innerHTML = newString;
+            if (i >= stringP.length) {
+                clearInterval(printStr);
+                setTimeout(function () {
+                    let iS = stringP.length;
+                    let newStringS = "";
+                    const printStrS = setInterval(function () {
+                        newStringS = stringP.substring(0, iS--);
+                        document.getElementById("404").innerHTML = newStringS;
+                        if (iS <= 0) {
+                            setTimeout(function () {
+                                let i = 0;
+                                stringP = " Not Found";
+                                newString = "";
+                                const printStr = setInterval(function () {
+                                    newString += stringP[i++];
+                                    document.getElementById("404").innerHTML = newString;
+                                    if (i >= stringP.length) {
+                                        setTimeout(function () {
+                                            let iS = stringP.length;
+                                            let newStringS = "";
+                                            const printStrS = setInterval(function () {
+                                                newStringS = stringP.substring(0, iS--);
+                                                document.getElementById("404").innerHTML = newStringS;
+                                                if (iS <= 0) {
+                                                    clearInterval(printStrS);
+                                                    setTimeout(function (){
+                                                        return repeter();
+                                                    },500)
+                                                }
+                                            }, 140);
+                                        }, 3000)
+                                        clearInterval(printStr);
+                                    }
+                                }, 120);
+                            }, 500)
+                            clearInterval(printStrS);
+                        }
+                    }, 140);
 
-            }, 3000)
-        }
-    }, 120);
+                }, 3000)
+            }
+        }, 120);
+    }
 
+    repeter()
     return (
         <div className="d-container">
             <Helmet>
-                <title>404 - meow</title>
+                <title>404 - Meow</title>
             </Helmet>
             <span className="nf-text-container">
-                <h1 style={{margin: "0"}}>Page<span id="404"></span></h1>
+                <span className="nf-anim-container">
+                    <span className="nf-anim">Page </span>
+                    <span id="404"></span>
+                </span>
                 <h3 id="blinkId" className="blink" style={{fontSize: "38px", alignSelf: "center", margin: "0"}}>|</h3>
             </span>
+            <span className="nf-return">return to <a className="default-link" href="/">cd ..</a></span>
         </div>
     )
 }
