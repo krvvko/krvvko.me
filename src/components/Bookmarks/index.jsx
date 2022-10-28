1edccb9c3825981fda7ff53ada9b67718b60533b
krvvko <krvvko@gmail.com>
2022-10-27 21:06:00 -0400

    content added

--- src/components/Bookmarks/index.jsx
+++ src/components/Bookmarks/index.jsx
@@ -1,10 +1,22 @@
+import './index.css';
 import Title from "../Title";
+import Cd from "../Cd";
 
 const Bookmarks = () => {
 
     return (
-        <div>
+        <div className="d-container">
             <Title />
+            <h1>Bookmarks</h1>
+
+            <span className="date">Oct 15, 2022</span>
+            <p>
+                On this page you can find useful resources that I often
+                use during development, their use can greatly facilitate the process of creating a page.
+            </p>
+
+            <h3>Page is not completely done yet</h3>
+
         </div>
     )
 
