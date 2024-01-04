f776efd96e8d0afd25cd498bc013fdc32727373f
krvvko <krvvko@gmail.com>
2024-01-04 03:46:00 -0500

    added

---
+++ src/utils/AnimationContext.tsx
@@ -0,0 +1,47 @@
+import React, { createContext, useState, useContext, useCallback } from 'react';
+import { useNavigate } from "react-router-dom";
+import './css/RedirectAnimation.css'
+import {AnimationContextType, AnimationProviderProps} from "./interfaces";
+import {useReducedMotion} from "./ReducedMotionContext";
+
+const AnimationContext = createContext<AnimationContextType>({
+    startAnimation: (): void => {},
+    isInProcess: false
+});
+
+export const useAnimation = () => useContext(AnimationContext);
+
+export const AnimationProvider: React.FC<AnimationProviderProps> = ({ children }) => {
+    const [animationClass, setAnimationClass] = useState<string>('');
+    const [isInProcess, setIsInProcess] = useState<boolean>(false);
+    const { reducedMotion } = useReducedMotion()
+    const navigate = useNavigate();
+
+    const startAnimation = useCallback((to: string) => {
+        setAnimationClass('start');
+        setIsInProcess(true);
+        setTimeout(() => {
+            navigate(to);
+            setAnimationClass('end');
+            setTimeout(()=> {
+                setAnimationClass('');
+                setIsInProcess(false);
+            }, 1000)
+        }, 1000);
+    }, [navigate]);
+
+    return (
+        <AnimationContext.Provider value={{ startAnimation, isInProcess }}>
+            <div className={`animation ${animationClass} ${reducedMotion ? 'reduced-motion' : ''}`}>
+                <div className="animation-wrapper">
+                    <div className="animation-white"></div>
+                    <div className="animation-grey"></div>
+                    <div className="animation-black"></div>
+                    <div className="animation-grey"></div>
+                    <div className="animation-white"></div>
+                </div>
+            </div>
+            {children}
+        </AnimationContext.Provider>
+    );
+};
