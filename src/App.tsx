10928308eaf4556eef044a83f47d2ba0a2b14763
krvvko <krvvko@gmail.com>
2024-01-04 03:46:22 -0500

    initial update

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Route, Routes} from "react-router-dom";
+import About from "./components/About";
+import Main from "./components/Main";
+import Header from "./components/Header";
+import NotFound from "./components/NotFound";
+import Background from "./components/Background";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: React.FC = () => {
+    return (
+        <>
+            <Background />
+            <Header />
+            <main>
+                <Routes>
+                    <Route path="/" element={<Main />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="*" element={<NotFound />} />
+                </Routes>
+            </main>
+        </>
+    );
+};
 
 export default App;
